778ae80b6e13c05bc2f7bc06333a806b0e352f69
smith1appdeveloper <smith1appdeveloper@gmail.com>
2022-12-28 11:03:45 -0500

    Site Updates

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 
 ALLOWED_HOSTS = ['play2learn-online.herokuapp.com']
 #ALLOWED_HOSTS = ['127.0.0.1']
