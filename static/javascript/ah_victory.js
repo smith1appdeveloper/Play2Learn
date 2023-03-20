6ea85b4e30725c3e336a9a2af088a8879e2d087f
smith1appdeveloper <smith1appdeveloper@gmail.com>
2023-03-20 17:55:49 -0400

    q1.23.1 site updates

---
+++ static/javascript/ah_victory.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function() {
+   addScore = sessionStorage.getItem("addScore");
+   addScoreM = sessionStorage.getItem("addScoreM");
+
+   if (isNaN(addScore) && isNaN(addScoreM)) {
+      score.innerText = 0;
+
+   } else if(isNaN(addScore) && addScoreM > 0){
+      let score = document.getElementById('score');
+         score.innerText = addScoreM;
+
+   } else {
+      let score = document.getElementById('score');
+         score.innerText = addScore;
+   }
+
+   if (isNaN(addScoreM)) {
+      scoreM.innerText = 0;
+
+   } else {
+      let scoreM = document.getElementById('scoreM');
+      scoreM.innerText = addScoreM;
+   }
+
+});
