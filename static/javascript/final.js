34610eca74b89b77b869256dbef4187d38cd05a2
smith1appdeveloper <smith1appdeveloper@gmail.com>
2023-04-16 17:44:33 -0400

    q2.23.7 in game sound effect updates

--- static/javascript/final.js
+++ static/javascript/final.js
@@ -1,9 +1,13 @@
-const gameOver = new Audio(); gameOver.autoplay = true;
+//const gameOver = new Audio(); gameOver.autoplay = true;
 
-function unlockAudio() {
-   gameOver.src = '/static/audio/silence.wav';
-  }
+//function unlockAudio() {
+//   gameOver.src = '/static/audio/silence.wav';
+//}
 window.addEventListener('load', function() {
+   const gameOver = new Audio(); gameOver.autoplay = true;
+   function unlockAudio() {
+      gameOver.src = '/static/audio/silence.wav';
+   }
    addScore = sessionStorage.getItem("addScore");
    addScoreM = sessionStorage.getItem("addScoreM");
    op = sessionStorage.getItem("op");
@@ -14,35 +18,35 @@ window.addEventListener('load', function() {
 
 
    if (isNaN(addScore)) {
+      gameOver.src = '/static/audio/gameover.wav';
       let score = 0;
       score.innerText = addScore;
       let op = sessionStorage.getItem("op");
       let opFin = document.getElementById('op');
       opFin.innerText = op;
-      gameOver.src = '/static/audio/gameover.wav';
    } else {
+         gameOver.src = '/static/audio/gameover.wav';
          let score = document.getElementById('score');
          score.innerText = addScore;
          let op = sessionStorage.getItem("op");
          let opFin = document.getElementById('op');
          opFin.innerText = op;
-         gameOver.src = '/static/audio/gameover.wav';
       }
 
    if (isNaN(addScoreM)) {
+      gameOver.src = '/static/audio/gameover.wav';
       let scoreM = 0;
       scoreM.innerText = addScoreM;
       opM = sessionStorage.getItem("opM");
       let opFinM = document.getElementById('opM');
       opFinM.innerText = op;
-      gameOver.src = '/static/audio/gameover.wav';
    } else {
+         gameOver.src = '/static/audio/gameover.wav';
          let scoreM = document.getElementById('scoreM');
          scoreM.innerText = addScoreM;
          opM = sessionStorage.getItem("opM");
          let opFinM = document.getElementById('opM');
          opFinM.innerText = op;
-         gameOver.src = '/static/audio/gameover.wav';
       }
 
    });
