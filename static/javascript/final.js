60d55fdad13be3b208a4f69e30c85b399ed3819b
smith1appdeveloper <smith1appdeveloper@gmail.com>
2023-04-16 12:32:00 -0400

    q2.23.7 in game sound effect updates

--- static/javascript/final.js
+++ static/javascript/final.js
@@ -1,7 +1,16 @@
+const gameOver = new Audio(); gameOver.autoplay = true;
+
+function unlockAudio() {
+   gameOver.src = '/static/audio/silence.wav';
+  }
 window.addEventListener('load', function() {
    addScore = sessionStorage.getItem("addScore");
    addScoreM = sessionStorage.getItem("addScoreM");
    op = sessionStorage.getItem("op");
+   unlockAudio();
+   document.body.addEventListener('touchstart', function() {
+      unlockAudio();
+   });
 
 
    if (isNaN(addScore)) {
@@ -10,12 +19,14 @@ window.addEventListener('load', function() {
       let op = sessionStorage.getItem("op");
       let opFin = document.getElementById('op');
       opFin.innerText = op;
+      gameOver.src = '/static/audio/gameover.wav';
    } else {
          let score = document.getElementById('score');
          score.innerText = addScore;
          let op = sessionStorage.getItem("op");
          let opFin = document.getElementById('op');
          opFin.innerText = op;
+         gameOver.src = '/static/audio/gameover.wav';
       }
 
    if (isNaN(addScoreM)) {
@@ -24,12 +35,14 @@ window.addEventListener('load', function() {
       opM = sessionStorage.getItem("opM");
       let opFinM = document.getElementById('opM');
       opFinM.innerText = op;
+      gameOver.src = '/static/audio/gameover.wav';
    } else {
          let scoreM = document.getElementById('scoreM');
          scoreM.innerText = addScoreM;
          opM = sessionStorage.getItem("opM");
          let opFinM = document.getElementById('opM');
          opFinM.innerText = op;
+         gameOver.src = '/static/audio/gameover.wav';
       }
 
    });
