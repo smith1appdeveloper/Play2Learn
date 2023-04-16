fce047bfaaceb89e88e2f299d0463cc3ba0edf3e
smith1appdeveloper <smith1appdeveloper@gmail.com>
2023-04-16 18:04:31 -0400

    q2.23.7 in game sound effect updates

--- static/javascript/final.js
+++ static/javascript/final.js
@@ -1,21 +1,13 @@
-//const gameOver = new Audio(); gameOver.autoplay = true;
+const gameOver = new Audio(); gameOver.autoplay = true;
 
-//function unlockAudio() {
-//   gameOver.src = '/static/audio/silence.wav';
-//}
+function unlockAudio() {
+   gameOver.src = '/static/audio/silence.wav';
+}
 window.addEventListener('load', function() {
-   const gameOver = new Audio(); gameOver.autoplay = true;
-   function unlockAudio() {
-      gameOver.src = '/static/audio/silence.wav';
-   }
+   unlockAudio();
    addScore = sessionStorage.getItem("addScore");
    addScoreM = sessionStorage.getItem("addScoreM");
    op = sessionStorage.getItem("op");
-   unlockAudio();
-   document.body.addEventListener('touchstart', function() {
-      unlockAudio();
-   });
-
 
    if (isNaN(addScore)) {
       gameOver.src = '/static/audio/gameover.wav';
