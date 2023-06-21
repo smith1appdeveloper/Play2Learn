057db89531c0165c17d0ab9aecfdbd5d37ff51c2
smith1appdeveloper <smith1appdeveloper@gmail.com>
2023-06-21 08:00:37 -0400

    q2.23.24 general website updates

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-#DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = ['play2learn-online.herokuapp.com']
 #ALLOWED_HOSTS = ['127.0.0.1']
