9256af86c545b46912a62ad736cc46c49ea6a8cd
smith1appdeveloper <smith1appdeveloper@gmail.com>
2023-08-11 21:23:22 -0400

    q2.23.18 1st Twenty initial game install

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import AboutUsView, contact_form, HomePageView, LoginView, devView, betaView, Random_AceView, Random_Ace_GainView, Random_Ace_LoseView, Random_Ace_GainMView, Random_Ace_LoseMView, Who_am_IView, Who_am_I2View, Final_TallyView, Who_am_I_endView, Final_Tally_UpView, Final_Tally_DownView, game_overView, game_over_View, twentyView
+from .views import AboutUsView, contact_form, HomePageView, LoginView, devView, betaView, Random_AceView, Random_Ace_GainView, Random_Ace_LoseView, Random_Ace_GainMView, Random_Ace_LoseMView, Who_am_IView, Who_am_I_rulesView, Who_am_I2View, Final_TallyView, Who_am_I_endView, Final_Tally_UpView, Final_Tally_DownView, game_overView, game_over_View, twentyView
 
 app_name =  'pages'
 urlpatterns = [
@@ -16,6 +16,7 @@ urlpatterns = [
     path('random_ace_gainM/', Random_Ace_GainMView.as_view(), name='random_ace_gainM'),
     path('random_ace_loseM/', Random_Ace_LoseMView.as_view(), name='random_ace_loseM'),
     path('who_am_I/', Who_am_IView.as_view(), name='who_am_I'),
+    path('who_am_I_rules/', Who_am_I_rulesView.as_view(), name='who_am_I_rules'),
     path('who_am_I2/', Who_am_I2View.as_view(), name='who_am_I2'),
     path('final_tally/', Final_TallyView.as_view(), name='final_tally'),
     path('Who_am_I_end/', Who_am_I_endView.as_view(), name='Who_am_I_end'),
